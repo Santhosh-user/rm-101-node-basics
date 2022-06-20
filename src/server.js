68c1e7af8d393e22b22c8aa304ef442391690fc9
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-06-20 19:24:12 +0530

    1

--- src/server.js
+++ src/server.js
@@ -1,8 +1,48 @@
 // install and import express
 const express = () => {};
 let app = express();
+const mongoose = require("mongoose")
+const express = require("express")
+const path = require("path")
 
 // Code here
 
+const connect = () =>{
+    return mongoose.connect("http://localhost:3000/user.json")
+}
+
+const UserSchema = new mongoose.Schmea({
+    "id": {type: Number},
+    "first_name": {type: String},
+    "last_name": {type: String},
+    "email": {type: String},
+    "gender": {type: String},
+    "ip_address": {type: String},
+    "age": {type: Number}
+})
+
+const User = mongoose.model("user", UserSchema)
+
+app.get("/",async(req,res)=>{
+    const user = await User.find().lean().exec()
+    return res.send(user)
+})
+
+app.get("/",async(req,res)=>{
+    const user = await User.find().lean().exec()
+    return res.send(user)
+})
+
 // Note: Do not remove this export statement
+
+app.listen(8000,async ()=>{
+    try{
+        await connect()
+        console.log("listening on port 8000")
+    }
+    catch(err){
+        console.error(err.message)
+    }
+})
+
 module.exports = app;
