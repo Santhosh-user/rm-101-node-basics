864edc904271b704c045b8d97ecec0bdbb43e8ad
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-06-20 19:42:51 +0530

    1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // install and import express
-const express = () => {};
+
 let app = express();
 const mongoose = require("mongoose")
 const express = require("express")
@@ -10,6 +10,8 @@ const path = require("path")
 // const connect = () =>{
 //     return mongoose.connect("http://localhost:3000/user.json")
 // }
+
+app.use(express.json())
 
 const UserSchema = new mongoose.Schmea({
     "id": {type: Number},
@@ -23,16 +25,41 @@ const UserSchema = new mongoose.Schmea({
 
 const User = mongoose.model("user", UserSchema)
 
+
 app.get("/",async(req,res)=>{
     try{
-        
+        var fileu = await path.join(__dirname+"/assets/users.html")
+        return res.sendFile(fileu)
+    }
+    catch(err){
+        return res.send(err)
     }
 })
 
-app.get("/",async(req,res)=>{
-    const user = await User.find().lean().exec()
-    return res.send(user)
+
+app.get("/users",async(req,res)=>{
+    try{
+        var jsony = await path.join(__dirname+"/assets/users.json")
+        return res.sendFile(jsony)
+    }
+    catch(err){
+        return res.send(err)
+    }
 })
+
+app.get("/users:id",async(req,res)=>{
+    try{
+        var userbid = await user.
+
+        })
+    }
+    catch(err){
+        return res.send(err)
+    }
+})
+
+
+
 
 // Note: Do not remove this export statement
 
